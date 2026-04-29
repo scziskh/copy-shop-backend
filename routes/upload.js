77b733648b858bd199df2f6f3973f8f07708d646
Sciskh <muzychukserhii@gmail.com>
2026-04-29 18:42:05 +0300

    env for cloudinary

--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,26 @@
-const uploadRoute = (req, res) => {
-  return res.json({ message: "OK", filename: req.file.filename });
-};
+const cloudinary = require("../lib/cloudinary");
 
-module.exports = uploadRoute;
+module.exports = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "Файл не завантажено" });
+    }
+
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { folder: "copy-shop-uploads" }, // папка в Cloudinary
+      (error, result) => {
+        if (error) {
+          console.error("Cloudinary Error:", error);
+          return res
+            .status(500)
+            .json({ error: "Помилка завантаження в хмару" });
+        }
+        res.status(200).json({ url: result.secure_url });
+      },
+    );
+
+    uploadStream.end(req.file.buffer);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
